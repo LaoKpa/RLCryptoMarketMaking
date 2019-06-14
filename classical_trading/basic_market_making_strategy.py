dcad4e8e374fbca0778847c01edbcfb880418fc2
Lavi Shapiro <lavi1993@gmail.com>
2019-06-14 12:28:12 +0300

    update

--- classical_trading/basic_market_making_strategy.py
+++ classical_trading/basic_market_making_strategy.py
@@ -26,3 +26,26 @@ class BasicMarketMakingStrategy:
         print('Order Elapsed: {0}.', t2-t1)
         import pudb; pudb.set_trace()
         print('duck')
+
+# 
+# {'exchange': 'bitfinex',
+#  'is_cancelled': False,
+#  'gid': None,
+#  'timestamp': '1560266870.53677338',
+#  'price': '1.6459',
+#  'side': 'buy',
+#  'original_amount': '10.0',
+#  'avg_execution_price': '0.0',
+#  'symbol': 'etpusd',
+#  'id': 26557157187,
+#  'is_live': True,
+#  'cid_date': '2019-06-11',
+#  'order_id': 26557157187,
+#  'was_forced': False,
+#  'oco_order': None,
+#  'remaining_amount': '10.0',
+#  'executed_amount': '0.0',
+#  'cid': 55670522797,
+#  'type': 'limit',
+#  'src': 'api',
+#  'is_hidden': False}
